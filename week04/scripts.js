2ea4f5e9a101c9a58c415c7fea3f1250cf9e1266
NurSaadahAikban <redxavier9@gmail.com>
2020-12-06 10:04:28 +0800

    addpawd

--- week04/scripts.js
+++ week04/scripts.js
@@ -5,3 +5,15 @@ function transformUpperCase(){
 
 let elUsername = document.getElementById("username");
 elUsername.addEventListener("keyup", transformUpperCase);
+
+function checkPasswordLength(){
+    let password = document.getElementById("password").value;
+    if(password.length <5){
+        alert("Password must be more than 5 characters");
+    }else{
+        alert("Password accepted.");
+    }
+}
+
+let elPassword = document.getElementById("password");
+elPassword.onblur = checkPasswordLength;
