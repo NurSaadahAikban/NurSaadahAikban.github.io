97e17b8b1b4df71d0c0e75f121fa0852781593e5
NurSaadahAikban <redxavier9@gmail.com>
2020-12-09 15:41:17 +0800

    seekbar

--- week05/scripts/video.js
+++ week05/scripts/video.js
@@ -10,4 +10,22 @@ window.onload = function(){
     pausebtn.addEventListener("click", function(){
         video.pause();
     });
-};
+
+    let freezebtn = document.getElementById("freezebtn");
+    freezebtn.addEventListener("mousedown", function(){
+        video.pause();
+    });
+    freezebtn.addEventListener("mouseup", function(){
+        video.play();
+    }); 
+
+    let seekbar = document.getElementById("seekbar");
+    seekbar.addEventListener("change", function(){
+        let time = video.duration *(seekbar.value /100);
+        video.currentTime = time;
+    });
+    seekbar.addEventListener("timeupdate", function(){
+        let value = (100 / video.duration) * video.currentTime;
+        seekbar.value=value;
+    });
+}
