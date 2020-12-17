5045224928989ba8a780bbe0f338c920910b0ae5
NurSaadahAikban <redxavier9@gmail.com>
2020-12-17 10:11:12 +0800

    index6

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -8,8 +8,16 @@ var app=new Vue({
 let app2 = new Vue({
     el:"#app2",
     data:{
-        message: "Your loaded this page on "+ new Date().toLocaleString()
+        message: "Your loaded this page on "+ new Date().toLocaleString(),
+        linkURL: "https://bbc.in/2LAnJs8",
+        linkText: "Google is down!"
 
+    }
+});
 
+let app3 = new Vue({
+    el:"#app3",
+    data:{
+        seen:true
     }
 });
