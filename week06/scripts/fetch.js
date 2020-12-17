e676950513080a70dd541b62331a954480abb68e
NurSaadahAikban <redxavier9@gmail.com>
2020-12-17 15:52:24 +0800

    FETCH7

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -14,7 +14,7 @@ function GetBookings(){
           let gId = json.bookings[x].id;
 
           bookingList.innerHTML += gId + "-" + gName + ", "+
-          gEmail + ", pax:" + pax + "<br>"; 
+          gEmail + ", gPax:" + pax + "<br>"; 
       }
     
     }); 
