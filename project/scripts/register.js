c1b67520857ea8b15aa512ecbd82f892b765210c
NurSaadahAikban <redxavier9@gmail.com>
2020-12-25 15:39:15 +0800

    register1111

--- project/scripts/register.js
+++ project/scripts/register.js
@@ -13,7 +13,6 @@ elUsername.onblur = checkUsernameLength;
 
 //how to check the email have mistype of @mail.com/ or not
 
-
 function checkPasswordLength(txt){
     if(txt.length < 10){
         alert("Your password is not strong enough. Make it more than 10 to make it strong.");
@@ -29,15 +28,54 @@ elPassword.addEventListener("blur", function(){
 });
 
 //matching it not working
-function checkPasswordMatch(elPassword){
-    let conpassword = document.getElementById("conpassword").value;
+//function checkPasswordMatch(elPassword){
+    //let conpassword = document.getElementById("conpassword").value;
 
-    if(conpassword != elPassword){
-        alert("Your password is not match! Please try again.");
-    }
-    else{
-        alert("Your password is match.");
-    }
-}
+    //if(conpassword != elPassword){
+        //alert("Your password is not match! Please try again.");
+    //}
+    //else{
+        //alert("Your password is match.");
+    //}
+//}
+
 let elConpassword = document.getElementById("conpassword");
 elConpassword.onblur = checkPasswordMatch;
+
+
+//x siap sbb sheety.co problem
+function Register(gname, gemail,gpassword){
+    let url = 'https://api.sheety.co/e074bea20583dc3b71f7c5f6b29b9973/nsa/bookings';
+    //let url = 'https://api.sheety.co/e074bea20583dc3b71f7c5f6b29b9973/bookingApp/bookings';
+  
+    let body = {
+      member: {
+        name:gname,
+        email:gemail,
+        password : gpassword
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+          "Content-Type": "application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then(json => {        
+        alert(json.member.name + " added");        
+      });
+  }
+  
+  window.addEventListener("load", function(){
+      document.getElementById("register").addEventListener("click", function(){
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+  
+        Register(name,email,password);
+  
+      });
+    });
