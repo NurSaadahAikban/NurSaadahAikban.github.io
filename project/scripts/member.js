f67bab136ec5703924fa4a2adb208b3f3673d11c
NurSaadahAikban <redxavier9@gmail.com>
2020-12-26 12:52:09 +0800

    asaasaa

---
+++ project/scripts/member.js
@@ -0,0 +1,26 @@
+function GetMembership(){
+    let url = 'https://api.sheety.co/e074bea20583dc3b71f7c5f6b29b9973/memberNsa/members';
+    fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+    // Do something with the data
+    let member = document.getElementById("member")
+    let memberIds =[];
+    
+    for(let x=0; x<json.members.length;x++){
+        let userinfo = "Member Id: " + json.members[x].id +"<br>Name: "+
+        json.members[x].name + "<br>Email: " + json.members[x].email +
+        "<br>Mobile: " +json.members[x].phone + "<br>Address: "+
+        json.members[x].address+" "+json.members[x].poscode +" "+
+        json.members[x].state + " "+ json.members[x].city;
+
+        row.insertCell(0).innerHTML = userinfo;
+    }
+    });
+}
+
+window.addEventListener("load", function(){
+    document.getElementById("membership").addEventListener("click", function(){
+        GetMembership();
+    });
+})
