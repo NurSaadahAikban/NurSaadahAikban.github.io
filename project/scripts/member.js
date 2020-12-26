d1b2c173fb5f264e50f7855b9d775bb652f5daed
NurSaadahAikban <redxavier9@gmail.com>
2020-12-26 13:11:19 +0800

    rere

--- project/scripts/member.js
+++ project/scripts/member.js
@@ -1,26 +1,33 @@
-function GetMembership(){
+function GetMember(){
+    
     let url = 'https://api.sheety.co/e074bea20583dc3b71f7c5f6b29b9973/memberNsa/members';
+    
     fetch(url)
     .then((response) => response.json())
     .then(json => {
-    // Do something with the data
-    let member = document.getElementById("member")
-    let memberIds =[];
-    
-    for(let x=0; x<json.members.length;x++){
-        let userinfo = "Member Id: " + json.members[x].id +"<br>Name: "+
-        json.members[x].name + "<br>Email: " + json.members[x].email +
-        "<br>Mobile: " +json.members[x].phone + "<br>Address: "+
-        json.members[x].address+" "+json.members[x].poscode +" "+
-        json.members[x].state + " "+ json.members[x].city;
+      // Do something with the data
+      //console.log(json.bookings);
+      let memberList = document.getElementById("memberList");
+      let bookingIds =[];
 
-        row.insertCell(0).innerHTML = userinfo;
-    }
-    });
+      for(let x=0; x<json.members.length;x++){          
+          let userinfo = "Membership Id: "+json.members[x].id + "<br>Name: " + json.members[x].name + 
+          "<br>Email: "+ json.members[x].email+"<br>Mobile: "+json.members[x].phone +"<br>Address: "+ 
+          json.members[x].address +" " + json.members[x].poscode + " "+ json.members[x].state +" " + json.members[x].city;
+          
+          let row = memberList.insertRow(memberList.rows.length);
+
+          //specify what to put in cell
+          row.insertCell(0).innerHTML = userinfo;
+          
+          //push all buttonid in 1 array
+          bookingIds.push(buttonId);
+      }      
+    }); 
 }
 
 window.addEventListener("load", function(){
     document.getElementById("membership").addEventListener("click", function(){
-        GetMembership();
-    });
+        GetMember();
+    });    
 })
