372952ac62d5b8d8f9e9d2344453ce927a5c6527
NurSaadahAikban <redxavier9@gmail.com>
2020-12-26 13:34:54 +0800

    asdaas

--- project/scripts/member.js
+++ project/scripts/member.js
@@ -8,26 +8,30 @@ function GetMember(){
       // Do something with the data
       //console.log(json.bookings);
       let memberList = document.getElementById("memberList");
-      let bookingIds =[];
+      
+        
+        for(let x=0; x<json.members.length;x++){          
+            let userinfo = "Membership Id: "+json.members[x].id + "<br>Name: " + json.members[x].name + 
+            "<br>Email: "+ json.members[x].email+"<br>Mobile: "+json.members[x].phone +"<br>Address: "+ 
+            json.members[x].address +" " + json.members[x].poscode + " "+ json.members[x].state +" " + json.members[x].city;
+            
+            let row = memberList.insertRow(memberList.rows.length);
 
-      for(let x=0; x<json.members.length;x++){          
-          let userinfo = "Membership Id: "+json.members[x].id + "<br>Name: " + json.members[x].name + 
-          "<br>Email: "+ json.members[x].email+"<br>Mobile: "+json.members[x].phone +"<br>Address: "+ 
-          json.members[x].address +" " + json.members[x].poscode + " "+ json.members[x].state +" " + json.members[x].city;
-          
-          let row = memberList.insertRow(memberList.rows.length);
-
-          //specify what to put in cell
-          row.insertCell(0).innerHTML = userinfo;
-          
-          //push all buttonid in 1 array
-          bookingIds.push(buttonId);
-      }      
+            //specify what to put in cell
+            row.insertCell(0).innerHTML = userinfo;
+            
+            
+        }      
     }); 
 }
 
 window.addEventListener("load", function(){
     document.getElementById("membership").addEventListener("click", function(){
-        GetMember();
+        if(GetMember() == null){
+            alert("Sorry, you are not a member yet.");            
+        }
+        else{
+            GetMember();
+        }
     });    
 })
